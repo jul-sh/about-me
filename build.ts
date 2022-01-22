42298fcaef223bb61131dd4796af3fce9e61336d
Juliette Pretot <hi@juliette.sh>
2022-01-22 16:00:37 +0000

    reorder functions for readability

--- build.ts
+++ build.ts
@@ -27,52 +27,12 @@ async function copyDir(source: string, destination: string) {
   }
 }
 
-function createHTMLPage(
-  htmlName: string,
-  htmlSegment: string,
-  css: string,
-): string {
-  return `<!DOCTYPE html>
-  <html lang="en">
-    <head>
-      <meta charset="utf-8" />
-      <meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no" />
-      <title>${
-    htmlName === "index" ? "" : `${htmlName} — `
-  }Juliette Pretot</title>
-      <meta name="description" content="Engineer at Google" />
-      <style>${css}</style>
-      <link rel="apple-touch-icon" sizes="180x180" href="./static/apple-touch-icon.png" />
-      <link rel="icon" type="image/png" sizes="32x32" href="./static/favicon-32x32.png" />
-      <meta name="theme-color" content="#101723" />
-    </head>
-    <body>
-      <div id="content-wrapper" class="page-${htmlName}">${
-    htmlName === "index"
-      ? `<picture>
-          <div class="image-placeholder"></div>
-          <source type="image/webp" srcset="./static/me-4by5.webp">
-          <source type="image/jpeg" srcset="./static/me-4by5.jpg">
-          <img src="./static/me-4by5.jpg" alt="Juliette in front of the Golden Gate bridge" width="100%"></img>
-        </picture>
-        <main>${htmlSegment}</main>`
-      : htmlSegment
-  }</div>
-    </body>
-  </html>`;
-}
-
-function markdownPathToHtmlName(path: string): string {
-  const extensionLessPath = path.substr(0, path.length - 3).toLocaleLowerCase();
-  return extensionLessPath === "readme" ? "index" : extensionLessPath;
-}
-
 const BUILD_DIR = "./build";
-const CSS = await Deno.readTextFile("./main.css");
-
 await emptyDir(BUILD_DIR);
 await Deno.mkdir(`${BUILD_DIR}/static`);
 await copyDir("./static", `${BUILD_DIR}/static`);
+
+const css = await Deno.readTextFile("./main.css");
 
 const LocalMarkdownFiles = new Set<string>();
 for await (const { isFile, name } of Deno.readDir("./")) {
@@ -82,12 +42,19 @@ for await (const { isFile, name } of Deno.readDir("./")) {
 }
 
 class JuliettesMarkdownRenderer extends Renderer implements Renderer {
+  static markdownPathToHtmlName(path: string): string {
+    const extensionLessPath = path.substr(0, path.length - 3)
+      .toLocaleLowerCase();
+    return extensionLessPath === "readme" ? "index" : extensionLessPath;
+  }
   link(
     ...[href, ...rest]: Parameters<Renderer["link"]>
   ): ReturnType<Renderer["link"]> {
     const normalizedPath = normalize(href);
     if (LocalMarkdownFiles.has(normalizedPath)) {
-      const htmlHref = `${markdownPathToHtmlName(normalizedPath)}.html`;
+      const htmlHref = `${
+        JuliettesMarkdownRenderer.markdownPathToHtmlName(normalizedPath)
+      }.html`;
       return super.link(htmlHref, ...rest);
     } else {
       return super.link(href, ...rest);
@@ -100,11 +67,40 @@ for (const markdownFilePath of LocalMarkdownFiles.values()) {
   const markdown = await Deno.readTextFile(markdownFilePath);
   const htmlSegment = Marked.parse(markdown)
     .content;
-  const htmlName = markdownPathToHtmlName(markdownFilePath);
-  const htmlPage = createHTMLPage(htmlName, htmlSegment, CSS);
+  const htmlName = JuliettesMarkdownRenderer.markdownPathToHtmlName(
+    markdownFilePath,
+  );
+
   await Deno.writeTextFile(
     `${BUILD_DIR}/${htmlName}.html`,
-    htmlPage,
+    `<!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <meta charset="utf-8" />
+      <meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no" />
+      <title>${
+      htmlName === "index" ? "" : `${htmlName} — `
+    }Juliette Pretot</title>
+      <meta name="description" content="Engineer at Google" />
+      <style>${css}</style>
+      <link rel="apple-touch-icon" sizes="180x180" href="./static/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="./static/favicon-32x32.png" />
+      <meta name="theme-color" content="#101723" />
+    </head>
+    <body>
+      <div id="content-wrapper" class="page-${htmlName}">${
+      htmlName === "index"
+        ? `<picture>
+          <div class="image-placeholder"></div>
+          <source type="image/webp" srcset="./static/me-4by5.webp">
+          <source type="image/jpeg" srcset="./static/me-4by5.jpg">
+          <img src="./static/me-4by5.jpg" alt="Juliette in front of the Golden Gate bridge" width="100%"></img>
+        </picture>
+        <main>${htmlSegment}</main>`
+        : htmlSegment
+    }</div>
+    </body>
+  </html>`,
     { create: true },
   );
 }
